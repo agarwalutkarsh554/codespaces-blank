d7022e66b15e065da245db7fe221d93ae4d8c407
Utkarsh Agarwal <37051428+agarwalutkarsh554@users.noreply.github.com>
2025-03-24 20:36:45 +0000

    modified:   package-lock.json modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Portfolio from "./Portfolio";
+import "./tailwind.css";
 import "./index.css";
 ReactDOM.render(<Portfolio />, document.getElementById("root"));
